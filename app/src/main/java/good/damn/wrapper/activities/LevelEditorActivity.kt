23e3d583c1d007474d255394d2df3ecd807adf5b
GoodDamn <grigorydum@gmail.com>
2025-07-18 18:14:28 +0300

    app:: LevelEditorActivity: full screen mode

--- app/src/main/java/good/damn/wrapper/activities/LevelEditorActivity.kt
+++ app/src/main/java/good/damn/wrapper/activities/LevelEditorActivity.kt
@@ -2,9 +2,17 @@ package good.damn.wrapper.activities
 
 import android.annotation.SuppressLint
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import good.damn.engine.interfaces.MGIListenerOnGetUserContent
 import good.damn.engine.interfaces.MGIRequestUserContent
 import good.damn.engine.opengl.models.MGMUserContent
@@ -37,6 +45,38 @@ ActivityResultCallback<Uri?>, MGIRequestUserContent {
             context,
             context
         )
+
+        val windowController = WindowCompat.getInsetsController(
+            window,
+            window.decorView
+        )
+
+        windowController.systemBarsBehavior = WindowInsetsControllerCompat
+            .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            window.decorView
+        ) { view, windowInsets ->
+
+            windowController.hide(
+                WindowInsetsCompat.Type.systemBars()
+            )
+
+            return@setOnApplyWindowInsetsListener ViewCompat
+                .onApplyWindowInsets(
+                    view,
+                    windowInsets
+                )
+        }
 
         setContentView(
             LevelEditorView(
