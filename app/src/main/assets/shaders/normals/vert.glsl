3fe33c3ef4ad38c71d9aaa1cd381c98423060f34
GoodDamn <grigorydum@gmail.com>
2025-08-21 10:39:10 +0300

    engine:: normal mode shaders

--- app/src/main/assets/shaders/normals/vert.glsl
+++ app/src/main/assets/shaders/normals/vert.glsl
@@ -6,11 +6,15 @@ uniform mat4 projection;
 uniform mat4 model;
 uniform mat4 view;
 
+varying vec3 outNormal;
+
 void main() {
 
     vec3 spaceWorld = vec3(model * position);
     mat4 spaceView = view * model;
     mat4 spaceClip = projection * spaceView;
+
+    outNormal = normal;
 
     gl_Position = spaceClip * position;
 }
--- app/src/main/assets/shaders/wireframe/frag.glsl
+++ app/src/main/assets/shaders/wireframe/frag.glsl
@@ -1,8 +1,5 @@
 precision mediump float;
 
-varying vec3 outNormal;
-varying vec2 outTexCoords;
-
 void main() {
     gl_FragColor = vec4(0.5);
 }
