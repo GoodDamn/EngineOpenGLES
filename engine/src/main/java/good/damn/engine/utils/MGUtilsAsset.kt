5d74bd1d5a6abac0b9e2ad6bbd13f1073ea9960f
GoodDamn <grigorydum@gmail.com>
2025-10-05 06:15:05 +0300

    engine:: MGUtilsAsset: load shader code from public directory

--- engine/src/main/java/good/damn/engine/utils/MGUtilsAsset.kt
+++ engine/src/main/java/good/damn/engine/utils/MGUtilsAsset.kt
@@ -1,15 +1,39 @@
 package good.damn.engine.utils
 
+import android.util.Log
 import good.damn.engine.MGEngine
+import java.io.File
+import java.io.FileInputStream
 
 class MGUtilsAsset {
 
     companion object {
+        private const val TAG = "MGUtilsAsset"
+
+        fun createShaderPublicDir() {
+            val dir = File(
+                MGEngine.DIR_PUBLIC,
+                "shaders"
+            )
+
+            if (!dir.exists() && dir.mkdirs()) {
+                Log.d(TAG, "copyShaderCodeToExternal: ${dir.name} is created")
+            }
+        }
 
         fun loadString(
             path: String
         ): String {
-            val inp = MGEngine.ASSETS.open(
+            val pubFile = File(
+                MGEngine.DIR_PUBLIC,
+                path
+            )
+
+            val inp = if (
+                pubFile.exists()
+            ) FileInputStream(
+                pubFile
+            ) else MGEngine.ASSETS.open(
                 path
             )
 
