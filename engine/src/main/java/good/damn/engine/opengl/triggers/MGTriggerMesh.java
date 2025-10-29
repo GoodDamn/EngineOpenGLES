66b2c49ebb425b747ee03f94e5d9c4129b76a92b
GoodDamn <grigorydum@gmail.com>
2025-10-29 08:34:55 +0300

    engine:: MGTriggerMeshGroup: move createFromObjects() content to MGTriggerMesh.createFromObject

--- engine/src/main/java/good/damn/engine/opengl/triggers/MGTriggerMesh.java
+++ engine/src/main/java/good/damn/engine/opengl/triggers/MGTriggerMesh.java
@@ -1,19 +1,29 @@
 package good.damn.engine.opengl.triggers;
 
+import android.opengl.GLES30;
 import android.util.Pair;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import good.damn.engine.opengl.MGArrayVertex;
+import good.damn.engine.opengl.MGObject3d;
 import good.damn.engine.opengl.MGVector;
+import good.damn.engine.opengl.drawers.MGDrawerMeshOpaque;
 import good.damn.engine.opengl.drawers.MGDrawerModeSwitch;
 import good.damn.engine.opengl.drawers.MGDrawerVertexArray;
+import good.damn.engine.opengl.entities.MGMaterial;
 import good.damn.engine.opengl.entities.MGMesh;
+import good.damn.engine.opengl.enums.MGEnumTextureType;
 import good.damn.engine.opengl.matrices.MGMatrixScaleRotation;
 import good.damn.engine.opengl.matrices.MGMatrixTransformationInvert;
 import good.damn.engine.opengl.matrices.MGMatrixTransformationNormal;
+import good.damn.engine.opengl.pools.MGPoolTextures;
+import good.damn.engine.opengl.shaders.MGIShaderTexture;
 import good.damn.engine.opengl.shaders.MGShaderDefault;
+import good.damn.engine.opengl.shaders.MGShaderMaterial;
 import good.damn.engine.opengl.shaders.MGShaderSingleMode;
+import good.damn.engine.opengl.textures.MGTexture;
 import good.damn.engine.opengl.triggers.callbacks.MGManagerTriggerStateCallback;
 import good.damn.engine.opengl.triggers.methods.MGTriggerMethodBox;
 import good.damn.engine.opengl.triggers.stateables.MGDrawerTriggerStateable;
@@ -38,6 +48,64 @@ public final class MGTriggerMesh {
         this.matrix = matrix;
         this.mesh = mesh;
         this.triggerState = triggerState;
+    }
+
+    @NonNull
+    public static MGTriggerMesh createFromObject(
+        @NonNull final MGObject3d obj,
+        @NonNull final MGShaderDefault shaderDefault,
+        @NonNull final MGPoolTextures poolTextures,
+        @NonNull final MGDrawerVertexArray drawVertBox,
+        @NonNull final MGShaderSingleMode shaderWireframe,
+        @NonNull final MGITrigger triggerAction
+    ) {
+        final MGArrayVertex arrayVertex = new MGArrayVertex();
+        arrayVertex.configure(
+            obj.vertices,
+            obj.indices,
+            MGArrayVertex.STRIDE
+        );
+
+        @NonNull final MGShaderMaterial mat = shaderDefault
+            .getMaterial();
+
+        @NonNull final MGTexture textureDiffuse = obj.texturesDiffuseFileName == null ?
+            poolTextures.getDefaultTexture()
+            : loadTextureCached(
+            poolTextures,
+            mat.getTextureDiffuse(),
+            MGEnumTextureType.DIFFUSE,
+            obj.texturesDiffuseFileName[0]
+        );
+
+        @NonNull final MGTexture textureMetallic = obj.texturesMetallicFileName == null ?
+            poolTextures.getDefaultTextureMetallic()
+            : loadTextureCached(
+            poolTextures,
+            mat.getTextureMetallic(),
+            MGEnumTextureType.METALLIC,
+            obj.texturesMetallicFileName[0]
+        );
+
+        return createFromVertexArray(
+            arrayVertex,
+            drawVertBox,
+            shaderDefault,
+            shaderWireframe,
+            new MGDrawerModeSwitch(
+                arrayVertex,
+                new MGDrawerMeshOpaque(
+                    arrayVertex,
+                    new MGMaterial(
+                        shaderDefault.getMaterial(),
+                        textureDiffuse,
+                        textureMetallic
+                    )
+                ),
+                GLES30.GL_CW
+            ),
+            triggerAction
+        );
     }
 
     @NonNull
@@ -117,5 +185,40 @@ public final class MGTriggerMesh {
             mesh,
             triggerState
         );
+    }
+
+    @NonNull
+    private static MGTexture loadTextureCached(
+        @NonNull final MGPoolTextures poolTextures,
+        @NonNull final MGIShaderTexture shaderTexture,
+        @NonNull final MGEnumTextureType textureType,
+        @NonNull final String textureName
+    ) {
+        @Nullable MGTexture texture = poolTextures.get(
+            textureName
+        );
+
+        if (texture != null) {
+            return texture;
+        }
+
+        try {
+            texture = MGTexture.Companion.createDefaultAsset(
+                textureName,
+                textureType,
+                shaderTexture
+            );
+
+            poolTextures.add(
+                textureName,
+                texture
+            );
+        } catch (Exception e) {
+            // file not found
+        }
+
+        return texture != null ?
+            texture
+            : poolTextures.getDefaultTexture();
     }
 }
