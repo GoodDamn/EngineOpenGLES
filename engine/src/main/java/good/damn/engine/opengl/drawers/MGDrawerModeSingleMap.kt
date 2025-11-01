93e64f788a4fa7770d1663241735d877ed12b910
GoodDamn <grigorydum@gmail.com>
2025-11-01 06:19:38 +0300

    engine:: MGDrawerModeSingleMap: create

---
+++ engine/src/main/java/good/damn/engine/opengl/drawers/MGDrawerModeSingleMap.kt
@@ -0,0 +1,22 @@
+package good.damn.engine.opengl.drawers
+
+import good.damn.engine.opengl.camera.MGCamera
+import good.damn.engine.opengl.shaders.MGShaderSingleMap
+import java.util.concurrent.ConcurrentLinkedQueue
+
+class MGDrawerModeSingleMap(
+    private val shaderMap: MGShaderSingleMap,
+    private val camera: MGCamera,
+    private val meshes: ConcurrentLinkedQueue<MGDrawerMeshSwitch>
+): MGIDrawer {
+
+    override fun draw() {
+        shaderMap.use()
+        camera.draw(
+            shaderMap
+        )
+        meshes.forEach {
+            it.draw()
+        }
+    }
+}
